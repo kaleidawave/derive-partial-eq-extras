912c68439a165ea95a62ede2e13bd3608586736c
Ben <kaleidawave@gmail.com>
2023-01-24 22:37:38 +0000

    Update to latest syn-helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,150 +1,156 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Span};
-use std::error::Error;
-use syn::{
-    parse_macro_input, parse_quote, BinOp, DeriveInput, Expr, ExprBinary, ExprLit, Lit, LitBool,
-    Stmt, Token, Type,
-};
 use syn_helpers::{
-    build_implementation_over_structure, BuildPair, CommaSeparatedList, Field, Fields,
-    PrefixAndPostfix, Trait, TraitMethod,
+    derive_trait,
+    proc_macro2::{Ident, Span},
+    quote,
+    syn::{
+        parse_macro_input, parse_quote, BinOp, DeriveInput, Expr, ExprBinary, ExprLit, Lit,
+        LitBool, Stmt, Token, Type,
+    },
+    CommaSeparatedList, Constructable, Field, FieldMut, Fields, HasAttributes, Trait, TraitItem,
 };
+
+const LEFT_NAME_POSTFIX: &str = "_left";
+const RIGHT_NAME_POSTFIX: &str = "_right";
 
 #[proc_macro_derive(
     PartialEqExtras,
     attributes(partial_eq_ignore_types, partial_eq_ignore)
 )]
 pub fn partial_eq_extras(input: TokenStream) -> TokenStream {
-    let structure = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let eq_item = TraitItem::new_method(
+        Ident::new("eq", Span::call_site()),
+        None,
+        syn_helpers::TypeOfSelf::Reference,
+        vec![parse_quote!(other: &Self)],
+        Some(parse_quote!(bool)),
+        |item| {
+            let ignored_types: Vec<Type> = item
+                .structure
+                .attributes()
+                .iter()
+                .filter(|attr| attr.path.is_ident(IGNORE_TYPES))
+                .flat_map(|attr| {
+                    attr.parse_args::<CommaSeparatedList<Type>>()
+                        .unwrap()
+                        .into_iter()
+                })
+                .collect();
+
+            match item.structure {
+                syn_helpers::Structure::Struct(r#struct) => {
+                    let expr =
+                        build_comparison_for_fields(r#struct.get_fields_mut(), &ignored_types);
+
+                    let left_patterns = r#struct.get_fields().to_pattern_with_config(
+                        r#struct.get_path(),
+                        syn_helpers::TypeOfSelf::Reference,
+                        LEFT_NAME_POSTFIX,
+                    );
+                    let right_patterns = r#struct.get_fields().to_pattern_with_config(
+                        r#struct.get_path(),
+                        syn_helpers::TypeOfSelf::Reference,
+                        RIGHT_NAME_POSTFIX,
+                    );
+                    let declaration = parse_quote! {
+                        let (#left_patterns, #right_patterns) = (self, other);
+                    };
+
+                    Ok(vec![declaration, Stmt::Expr(expr)])
+                }
+                syn_helpers::Structure::Enum(r#enum) => {
+                    let branches = r#enum.variants.iter_mut().map(|variant| {
+                        let expr =
+                            build_comparison_for_fields(variant.get_fields_mut(), &ignored_types);
+
+                        let left_patterns = variant.get_fields().to_pattern_with_config(
+                            variant.get_path(),
+                            syn_helpers::TypeOfSelf::Reference,
+                            LEFT_NAME_POSTFIX,
+                        );
+                        let right_patterns = variant.get_fields().to_pattern_with_config(
+                            variant.get_path(),
+                            syn_helpers::TypeOfSelf::Reference,
+                            RIGHT_NAME_POSTFIX,
+                        );
+                        let token_stream = quote! { (#left_patterns, #right_patterns) => #expr };
+                        token_stream
+                    });
+                    let match_stmt = parse_quote! {
+                        match (self, other) {
+                            #(#branches,)*
+                            (_, _) => false
+                        }
+                    };
+                    Ok(vec![match_stmt])
+                }
+            }
+        },
+    );
 
     // PartialEq trait
     let partial_eq_trait = Trait {
         name: parse_quote!(::std::cmp::PartialEq),
-        generic_parameters: vec![],
-        methods: vec![TraitMethod {
-            method_name: Ident::new("eq", Span::call_site()),
-            method_parameters: vec![parse_quote!(&self), parse_quote!(other: &Self)],
-            method_generics: vec![],
-            return_type: Some(parse_quote!(bool)),
-            build_pair: BuildPair::Pair {
-                statements_if_enums_do_not_match: vec![parse_quote!(return false;)],
-                other_item_name: Ident::new("other", Span::call_site()),
-            },
-        }],
+        generic_parameters: None,
+        items: vec![eq_item],
     };
 
-    build_implementation_over_structure(
-        &structure,
-        partial_eq_trait,
-        |_, _| Ok(PrefixAndPostfix::default()),
-        |method_name, fields| {
-            if method_name == "eq" {
-                partial_eq_extras_impl(fields)
-            } else {
-                unreachable!();
-            }
-        },
-    )
-    .into()
+    let derive_trait = derive_trait(input, partial_eq_trait);
+    derive_trait.into()
 }
 
 const IGNORE_TYPES: &str = "partial_eq_ignore_types";
 const IGNORE_FIELD: &str = "partial_eq_ignore";
 
-fn partial_eq_extras_impl(fields: &mut Fields) -> Result<Vec<Stmt>, Box<dyn Error>> {
-    let ignored_types: Vec<Type> = fields
-        .get_structure()
-        .all_attributes()
-        .find_map(|attr| {
-            if attr.path.is_ident(IGNORE_TYPES) {
-                Some(
-                    attr.parse_args::<CommaSeparatedList<Type>>()
-                        .unwrap()
-                        .into_iter()
-                        .collect::<Vec<_>>(),
-                )
-            } else {
-                None
-            }
+fn build_comparison_for_fields(fields: &mut Fields, ignored_types: &[Type]) -> Expr {
+    let mut top = None::<Expr>;
+
+    for mut field in fields.fields_iterator_mut() {
+        if field
+            .get_attributes()
+            .iter()
+            .any(|attr| attr.path.is_ident(IGNORE_FIELD))
+        {
+            continue;
+        }
+
+        let ignore_type_reference_and_on_tests =
+            ignored_types.iter().any(|ty| field.get_type() == ty);
+
+        if ignore_type_reference_and_on_tests {
+            continue;
+        }
+
+        let lhs = field.get_reference_with_config(true, LEFT_NAME_POSTFIX);
+        let rhs = field.get_reference_with_config(true, RIGHT_NAME_POSTFIX);
+
+        let expr = Expr::Binary(ExprBinary {
+            attrs: Vec::new(),
+            left: Box::new(lhs),
+            op: BinOp::Eq(Token!(==)(Span::call_site())),
+            right: Box::new(rhs),
+        });
+
+        if let Some(old_top) = top {
+            top = Some(Expr::Binary(ExprBinary {
+                attrs: Vec::new(),
+                left: Box::new(old_top),
+                op: BinOp::And(Token!(&&)(Span::call_site())),
+                right: Box::new(expr),
+            }));
+        } else {
+            top = Some(expr);
+        }
+    }
+    top.unwrap_or_else(|| {
+        Expr::Lit(ExprLit {
+            attrs: vec![],
+            lit: Lit::Bool(LitBool {
+                value: true,
+                span: Span::call_site(),
+            }),
         })
-        .unwrap_or_default();
-
-    let true_expr = Expr::Lit(ExprLit {
-        attrs: vec![],
-        lit: Lit::Bool(LitBool {
-            value: true,
-            span: Span::call_site(),
-        }),
-    });
-
-    let expr = match fields {
-        Fields::Named { fields, .. } => {
-            let mut top = None::<Expr>;
-            for field in fields {
-                if field
-                    .attrs
-                    .iter()
-                    .any(|attr| attr.path.is_ident(IGNORE_FIELD))
-                {
-                    continue;
-                }
-                if ignored_types.iter().any(|ty| ty == field.ty) {
-                    continue;
-                }
-                let (lhs, rhs) = (field.get_reference(), field.get_other_reference());
-                let expr = Expr::Binary(ExprBinary {
-                    attrs: Vec::new(),
-                    left: Box::new(lhs),
-                    op: BinOp::Eq(Token!(==)(Span::call_site())),
-                    right: Box::new(rhs),
-                });
-                if let Some(old_top) = top {
-                    top = Some(Expr::Binary(ExprBinary {
-                        attrs: Vec::new(),
-                        left: Box::new(old_top),
-                        op: BinOp::And(Token!(&&)(Span::call_site())),
-                        right: Box::new(expr),
-                    }));
-                } else {
-                    top = Some(expr);
-                }
-            }
-            top.unwrap_or(true_expr.clone())
-        }
-        Fields::Unnamed { fields, .. } => {
-            let mut top = None::<Expr>;
-            for field in fields {
-                if field
-                    .attrs
-                    .iter()
-                    .any(|attr| attr.path.is_ident(IGNORE_FIELD))
-                {
-                    continue;
-                }
-                if ignored_types.iter().any(|ty| ty == field.ty) {
-                    continue;
-                }
-                let (lhs, rhs) = (field.get_reference(), field.get_other_reference());
-                let expr = Expr::Binary(ExprBinary {
-                    attrs: Vec::new(),
-                    left: Box::new(lhs),
-                    op: BinOp::Eq(Token!(==)(Span::call_site())),
-                    right: Box::new(rhs),
-                });
-                if let Some(old_top) = top {
-                    top = Some(Expr::Binary(ExprBinary {
-                        attrs: Vec::new(),
-                        left: Box::new(old_top),
-                        op: BinOp::And(Token!(&&)(Span::call_site())),
-                        right: Box::new(expr),
-                    }));
-                } else {
-                    top = Some(expr);
-                }
-            }
-            top.unwrap_or(true_expr.clone())
-        }
-        Fields::Unit { .. } => true_expr,
-    };
-    Ok(vec![Stmt::Expr(expr)])
+    })
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use derive_partial_eq_extras::PartialEqExtras;
 
 #[derive(PartialEqExtras, Debug)]
